0656f8b9de3a16f0d383826d4f45027f6738da25
ljaybarr <Landrew.Barr@SDGKU.edu>
2024-08-06 22:52:05 -0500

    forms added

--- static/js/script.js
+++ static/js/script.js
@@ -1,49 +1,53 @@
 
-(function(){
+function addNewCategory(name) {
 
-    document.querySelector('#categoryInput').addEventListener('keydown', function(e){
-        if (e.keycode != 13){
-            return;
-        }
-
-        e.preventDefault()
-
-        var categoryName = this.value
-        this.value = ''
-        addNewCategory(categoryName)
-        updateCategoriesString()
-    })
-
-    function addNewCategory(name){
-
-        document.querySelector('#categoriesContainer').insertAdjacentHTML('beforeend',
+    document.querySelector('#categoriesContainer').insertAdjacentHTML('beforeend',
         `<li class="category">
             <span class="name">${name}</span>
             <span onclick="removeCategory(this)"class="btnRemove bold">X</span>
         </li>`)
-    }
+}
 
-    function fetchCategoryArray(){
-        var categories = []
+function fetchCategoryArray() {
+    var categories = []
 
-        document.querySelectorAll('.category').forEach(function(e){
-            name = e.querySelector('.name').innerHTML
-            if (name == '') return;
+    document.querySelectorAll('.category').forEach(function (e) {
+        let name = e.querySelector('.name').innerHTML
+        if (name == '') return;
 
-            categories.push(name)
-        })
+        categories.push(name)
+    })
 
-        return categories
-    }
+    return categories
+}
 
-   function updateCategoriesString(){
+function updateCategoriesString() {
     categories = fetchCategoryArray()
     document.querySelector('input[name="categoriesString"]').value = categories.join(',')
-   } 
+}
 
-})()
 
-function removeCategory(e){
+function removeCategory(e) {
     e.parentElement.remove()
     updateCategoriesString()
 }
+
+
+function init() {
+    console.log("JS works");
+
+    document.getElementById("projectForm").addEventListener("keydown", function (e) {
+        if (e.key == "Enter") {
+            if (e.target.name == "categoryInput") {
+                console.log("calling add");
+                const catName = e.target.value;
+                addNewCategory(catName)
+                updateCategoriesString()
+            }
+
+            e.preventDefault();
+        }
+    });
+}
+
+window.onload = init;
